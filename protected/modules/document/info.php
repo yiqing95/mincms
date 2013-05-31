4ff2cddf7e1fb704050e4e38f9f7e6ad7002fc2a
sunkang <68103403@qq.com>
2013-05-31 12:01:25 +0800

    test

--- protected/modules/document/info.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(  
-	'label'=>'document',
-	'memo'=>'手册',
- 
-);
--- protected/modules/oauth/Module.php
+++
@@ -1,6 +0,0 @@
-<?php 
-namespace app\modules\oauth; 
-class Module extends \yii\base\Module
-{
-	public $controllerNamespace = 'app\modules\oauth\controllers'; 
-}
