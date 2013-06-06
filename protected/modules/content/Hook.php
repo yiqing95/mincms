273d05bbd14f35b3d416a9c1d1d7ed64edf23fb5
mincms <68103403@qq.com>
2013-06-06 16:50:12 +0800

    remove content module

--- protected/modules/content/Hook.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace app\modules\content; 
-class Hook
-{
-	static function run($widget,$action){
-		$m = "app\modules\content\widget\\$widget\Hook";
-		return $m::$action();
-	}
-}
--- protected/modules/content/Module.php
+++
@@ -1,6 +0,0 @@
-<?php 
-namespace app\modules\content; 
-class Module extends \yii\base\Module
-{
-	public $controllerNamespace = 'app\modules\content\controllers'; 
-}
